905e56ec47097526fada3a9b8eb902c94011d5c1
heartflylee <2595672062@qq.com>
2018-04-15 15:07:12 +0800

    外呼标签

--- src/js/outboundLabel.js
+++ src/js/outboundLabel.js
@@ -3,6 +3,12 @@ var vm = new Vue({
   el: '#app',
   data: function() {
     return {
+      calltips:{
+        intentions: [{value:'有意向',id:'1'},{value: '有意向1',id:'2'}],
+        noIntentions: [{value:'无意向' ,id:"1"},{value:  '无意向1',id:"2"}],
+        dataErrors: [{value:'资料错误',  id:"1"},{value: '资料错误1',id:"2"}],
+        twiceCalls: [{value:'二次外呼',  id:"1"},{value: '二次外呼1',id:"2"}],
+      },
       layers: {
       },
     };
